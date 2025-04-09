a178f952ffeb446fac9f0cae7d247b957a3aa2e7
Yuragranovsky <yuragranovsky@gmail.com>
2025-04-09 21:34:56 +0300

    Specific TS types (any, unknown, index signature, arrays

--- src/main.ts
+++ src/main.ts
@@ -1,49 +1,26 @@
 import './style.css'
-//JS standard types
-let a = 10; //infering type
-//a = "kuku" //static types no allowed updating type
-let b: number;
 
-b = 10;
-console.log(b);
-let s: string = "10";
-let bl: boolean;
-bl = !!s;
-let n:null = null;
-let obj:{a: string} = {a: "kuku"};
-if(obj) {
-  //always true;
-}
-obj.a = "10";
-const c = "b";
-//alias 
 type Person = {id: number, name: string, city: string, age: number};
-let person: Person  = {id:123, age:25,
-   city: "Lod", name: "Vasya"};
-   console.log(person.age);
-   
-function createPerson(id: number, name: string, city: string, age: number): Person  {
-return {id,name, city, age};
-}
-const person1: Person = createPerson(123, "Kolya", "Lod", 30);
-person1.age = 40;
-function displayPersonAge(person: Person): void {
-  console.log(person.age);
+//any - not recommended 
+let a: any;
+a = 10;
+a = "kuku";
+a = undefined;
+a.a = 10;
+a = {};
+//unknown - safed any
 
+function display(obj: unknown) {
+  typeof obj === "number" && obj++;
+  console.log(obj);
+  let sub: string;
+  typeof obj === "string" &&(sub = obj.substring(1, 2));
 }
-function displayPersonName(person: Person): void {
-  console.log(person.name);
-}
-function displayKuku(num: number): number {
-  console.log(num);
-  return 5;
-}
-let displayPersonField:(person: Person) => void;
-displayPersonField = displayPersonAge;
-displayPersonField(person1);
-//displayPersonField = displayKuku; error
-displayPersonField(person);
-  
-   
+//index signature 
+let obj: {[key: number]:string} = {};
+obj[1] = "10";
+obj[2.6] = "w";
 
-
+//arrays
+let ar: number[] = [1, 2, 3];
+//ar.push(...[1, 2, "k"]); error
