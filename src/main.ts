8f57be20ba3df27741a7196233d61ee2718a274b
Yuragranovsky <yuragranovsky@gmail.com>
2025-04-20 20:56:43 +0300

    type mapping

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,21 @@
-type KeyValuePair<K, V> = {
-  key: K;
-  value: V;
-};
-
-let keyValuePairs = { key: 1, value: "value" };
-let keyValuePairs1: KeyValuePair<string, number> = { key: "abc", value: 100 };
-//generic constraint
-function findByAgeOrName<T extends {age: number, name: string}>(ar:T[], field: "age" | "name", value: unknown): T[] {
-  return ar.filter((item: any) => item[field] === value)
+//type mapping 
+type Person = {
+    name: string;
+    age: number;
 }
-const data = [
-  { name: "Jane", age: 20, gender:"female" },
-  { name: "John", age: 25, gender: "male" },
-  { name: "John", age: 30, gender: "male" }
-];
-const result = findByAgeOrName(data, "age", 20);
+type ReadOnly<T> = {
+  readonly[K in keyof T]: T[K] 
+}
+type Optional<T> = {
+  [K in keyof T]?: T[K] // K is the key of T, and T[K] is the value of T[K]
+}
+const person: Person = {
+    name: 'John',
+    age: 30
+};
+const readOnlyPerson: ReadOnly<Person> = {
+    name: 'John',
+    age: 30
+};
+person.age = 31; // OK
+//readOnlyPerson.age = 31; // Error: Cannot assign to 'age' because it is a read-only property
