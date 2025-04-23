ea324a198a792122a7bb6a8a9c38ec052e1ef155
Yuragranovsky <yuragranovsky@gmail.com>
2025-04-23 21:05:07 +0300

    Shapes

---
+++ src/Shape.ts
@@ -0,0 +1,6 @@
+export default interface Shape {
+    width: number;
+    height: number;
+    square(): number;
+    perimeter(): number;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,18 @@
-// const inputElement = document.getElementById("123") as HTMLInputElement;
-const inputElement = <HTMLInputElement>document.getElementById("123");
+import Container from "./Container";
+import Rectangle from "./Rectangle";
+import Shape from "./Shape";
+import Square from "./Square";
+
+const shape: Shape = new Rectangle(3, 4);
+console.log(shape.width, shape.square(), shape.perimeter());
+const shape2: Shape = new Square(4);
+console.log(shape2.height, shape2.perimeter(), shape2.square());
+const shape3: Shape = new Container([shape, shape2]);
+console.log(shape3.height, shape3.perimeter(), shape3.square());
+shape3 instanceof Container && console.log(shape3.size())
+shape2 instanceof Container && console.log(shape2.size())
+shape instanceof Container && console.log(shape.size())
+
 
 
 
