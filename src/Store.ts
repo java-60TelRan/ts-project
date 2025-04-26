ef97b9028ccaa100d285abee19899fc49aa63edf
Yuragranovsky <yuragranovsky@gmail.com>
2025-04-26 18:04:27 +0300

    HW #31 solution

--- src/Store.ts
+++ src/Store.ts
@@ -2,26 +2,31 @@ import Identifiable from "./Identifiable";
 import Predicate from "./Predicate";
 
 export default class Store<T extends Identifiable> {
-    items: T[] = [];
+    private items: T[] = [];
     add(item: T):void {
-       //TODO
        //adds new item with validation of unique id value
        //if item with the given id exists the method throws exception 
+       if(this.getById(item.id)) {
+           throw new Error(`Item with id ${item.id} already exists`);
+       }
+       this.items.push(item);
+       
     }
     getById(id: string): T | undefined {
-        //TODO
         //returns item with the given id or undefined if such item doesn't exist
-        return undefined;
+        return this.items.find(item => item.id === id);
     }
     find(predicate: Predicate<T>): T[] {
-        //TODO
         //returns array of items matching the given predicate
-        return [];
+        return this.items.filter(item => predicate.test(item));  
     }
     remove(id: string): T | undefined {
-        //TODO
-        //removes item with the given id value
-        //returns a reference to being removed item or undefined if an item doesn't exist
-        return undefined;
+        const index = this.items.findIndex(item => item.id === id);
+        let res = undefined
+        if (index > -1) {
+            [res] = this.items.splice(index, 1);
+        }
+        return res;
     }
+   
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 //TODO
 //Write simple test based on console.log functionality for testing Store class methods
 
+import Predicate from "./Predicate";
 import Store from "./Store";
 
 //For example T msay be class Employee like
@@ -9,5 +10,25 @@ class Employee {
 
     }
 }
+class SalaryGreaterThan implements Predicate<Employee> {
+    constructor(private minSalary: number) { }
+    test(item: Employee): boolean {
+        return item.salary > this.minSalary;
+    }
+}
 const store = new Store<Employee> ();
-//some test methods
+const emp1 = new Employee("1", "John Doe", 50000);
+const emp2 = new Employee("2", "Bob Smith", 60000);
+const emp3 = new Employee("3", "Alice Johnson", 70000);
+const emp4 = new Employee("4", "Bob Brown", 80000);
+const empleyees: Employee[] = [emp1, emp2, emp3, emp4];
+empleyees.forEach(emp => store.add(emp));
+
+console.log(store.getById("1"),'Should print Employee object with id "1"'); // Should print Employee object with id "1"
+console.log(store.getById("5"),'Should print undefined'); // Should print undefined
+console.log(store.find({  test:emp => emp.salary < 60000 }),'Should print array of Employee objects with salary less than 60000'); // Should print array of Employee objects with salary less than 60000  
+console.log(store.find(new SalaryGreaterThan(50000)),'Should print array of Employee objects with salary greater than 50000'); // Should print array of Employee objects with salary greater than 50000
+console.log(store.find({test: emp => emp.name.startsWith("Bob")}),'Should print Employees with ids: "2" and "4"'); // Should print Employees with ids: "2" and "4"
+console.log(store.find(new SalaryGreaterThan(80000)),'Should print empty array'); // Should print empty array
+console.log(store.remove("1"),'should print Employee with id "1" as result of removing employee with id "1"'); // Should remove Employee object with id "1"    
+console.log(store.getById("1"),'Should print undefined after removing Employee with id "1"'); // Should print undefined
